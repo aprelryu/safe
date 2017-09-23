29358fc0826b5b0e1f3ed6cbc9de7dbc8a76bb3d
aprelryu <32217738+aprelryu@users.noreply.github.com>
2017-09-23 05:25:47 -0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -22,11 +22,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="http://www.shinryu-safe.ga/2017/09/what-is-google-adsense.html"}
-if(x=="2"){xxx="http://www.shinryu-safe.ga/2017/09/blogging-for-profit-begins-with-long.html"}
-if(x=="3"){xxx="http://www.shinryu-safe.ga/2017/09/pengenalan-dasar-visual-basic.html"}
-if(x=="4"){xxx="http://www.shinryu-safe.ga/2017/09/topologi-jaringan-komputer.html"}
-if(x=="5"){xxx="http://www.shinryu-safe.ga/2017/09/where-adsense-sould-appear-on-blog.html"}a_to_ve[i].href="https://convert-safelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/2017/09/what-is-google-adsense.html"}
+if(x=="2"){xxx="/2017/09/blogging-for-profit-begins-with-long.html"}
+if(x=="3"){xxx="/2017/09/pengenalan-dasar-visual-basic.html"}
+if(x=="4"){xxx="/2017/09/topologi-jaringan-komputer.html"}
+if(x=="5"){xxx="/2017/09/where-adsense-sould-appear-on-blog.html"}a_to_ve[i].href="http://www.shinryu-safe.ga"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
